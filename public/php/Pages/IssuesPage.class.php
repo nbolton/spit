95bc02b6d85c0c5162b7745f8c97077e0ffe2a77
Nick Bolton <nick@synergy-project.org>
2012-05-23 23:08:02 +0000

    renamed factory to provider. changed path handling. added "new" view.

--- public/php/Pages/IssuesPage.class.php
+++ public/php/Pages/IssuesPage.class.php
@@ -4,9 +4,20 @@ namespace Spit\Pages;
 
 class IssuesPage extends Page {
   
-  public function run() {
+  public function run($path) {
+    
+    if (count($path) == 1) {
+      $this->showView("issues");
+    }
+    else {
+      switch (strtolower($path[1])) {
+        case "new": $this->runNew(); break;
+      }
+    }
+  }
   
-    $this->showView("issues");
+  private function runNew() {
+    $this->showView("issues_new");
   }
 }
 
