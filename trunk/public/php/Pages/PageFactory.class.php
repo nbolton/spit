25986506c1aa3ec15d0a37b487d8d22a19dd0e13
nick <nick@58fbd588-90f4-4515-9eb9-d00ffd0e213a>
2012-05-23 17:08:25 +0000

    added "path" parsing and htaccess

--- trunk/public/php/Pages/PageFactory.class.php
+++ trunk/public/php/Pages/PageFactory.class.php
@@ -4,6 +4,7 @@ namespace Spit\Pages;
 
 require "Page.class.php";
 require "HomePage.class.php";
+require "IssuesPage.class.php";
 
 use Exception;
 
@@ -12,7 +13,8 @@ class PageFactory {
   public function get($name) {
     switch($name) {
       case "home": $c = new HomePage; break;
-      default: throw new Exception("controller not found for: " . $name);
+      case "issues": $c = new IssuesPage; break;
+      default: throw new Exception("page not found for: " . $name);
     }
     return $c;
   }
