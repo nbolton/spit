fb8ea40a9e4902b558ca051f50566ac6bd7dc3d6
nick <nick@58fbd588-90f4-4515-9eb9-d00ffd0e213a>
2012-05-25 01:07:56 +0000

    moved settings. added database settings.

--- trunk/public/php/Spit/DataStores/DataStore.class.php
+++ trunk/public/php/Spit/DataStores/DataStore.class.php
@@ -7,7 +7,8 @@ use mysqli;
 class DataStore {
 
   public function getSql() {
-    $mysqli = new mysqli("localhost", "root", "", "spit");
+    $s = \Spit\Settings::$instance;
+    $mysqli = new mysqli($s->db->host, $s->db->user, $s->db->password, $s->db->database);
     if ($mysqli->connect_errno) {
       throw new Exception("failed to connect to mysql: " . $mysqli->connect_error);
     }
