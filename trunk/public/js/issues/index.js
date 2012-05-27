e67a38fa75c66259321b4d65abcf050908381ad9
nick <nick@58fbd588-90f4-4515-9eb9-d00ffd0e213a>
2012-05-27 20:49:30 +0000

    started on paging.

---
+++ trunk/public/js/issues/index.js
@@ -0,0 +1,35 @@
+/*
+ * SPIT: Simple PHP Issue Tracker
+ * Copyright (C) 2012 Nick Bolton
+ * 
+ * This package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * found in the file COPYING that should have accompanied this file.
+ * 
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function viewLoad() {
+  loadPagingInfo(1);
+}
+
+function loadPagingInfo(current) {
+  
+  log("loading paging info for: " + current);
+  
+  $.getJSON("?getPagingFor=" + current,
+  {
+    tags: "",
+    tagmode: "any",
+    format: "json"
+  },
+  function(data) {
+    log("done");
+  });
+}
